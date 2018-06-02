80a4de2b91073f64907eaf4b105cbbe1d757fe6c
Neeraj Jain <jaine03@ca.com>
2018-06-02 14:23:12 +0530

    Maximum Sum Such That No 2 Elements Are Adjacent

--- src/com/company/amazon/MaximumSumSuchThatNo2ElementsAreAdjacent.java
+++ src/com/company/amazon/MaximumSumSuchThatNo2ElementsAreAdjacent.java
@@ -1,33 +1,23 @@
 package com.company.amazon;
 
-public class MaximumSumSuchThatNo2ElementsAreAdjacent {
-    public static void main(String[] args) throws java.lang.Exception {
-//        findMaximumSum(new int[]{5, 5, 10, 100, 10, 5});
-//        findMaximumSum(new int[]{1, 2, 3});
-//        findMaximumSum(new int[]{1, 20, 3});
-//        findMaximumSum(new int[]{5, 5, 10, 40, 50, 35});
-//        findMaximumSum(new int[]{0, 24, 5, 6, 1, 43, 423, 54, 23, 54, 22, 6, 0, 1, 4, 2, 33, 2, 4, 2, 3});
-//        findMaximumSum(new int[]{1, 4, 1, 1, 7});
-//
-//
-//        System.out.println("Find Max Sum Optimized");
-//        findMaxSumOptimized(new int[]{5, 5, 10, 100, 10, 5});
-//        findMaxSumOptimized(new int[]{1, 2, 3});
-//        findMaxSumOptimized(new int[]{1, 20, 3});
-//        findMaxSumOptimized(new int[]{5, 5, 10, 40, 50, 35});
-//        findMaxSumOptimized(new int[]{0, 24, 5, 6, 1, 43, 423, 54, 23, 54, 22, 6, 0, 1, 4, 2, 33, 2, 4, 2, 3});
-//        findMaxSumOptimized(new int[]{1, 4, 1, 1, 7});
+import com.geeksforgeeks.array.ArrayRotation;
 
-        System.out.println("Find Max Sum Using DP .........the only correct solution :::===>" + findMaxSumUsingDP(new int[]{1, 4, 1, 1, 7}, 5));
+/**
+ * Problem : 146 Amazon Interview Question
+ */
+public class MaximumSumSuchThatNo2ElementsAreAdjacent {
+    public static void main(String[] args) {
+        findMaxSumUsingDP(new int[]{1, 4, 1, 1, 7}, 5);
+        findMaxSumUsingDP(new int[]{5, 5, 10, 100, 10, 5}, 6);
 
     }
 
     // Correct Solution
-    public static int findMaxSumUsingDP(int[] arr, int n) {
+    public static void findMaxSumUsingDP(int[] arr, int n) {
         if (n == 1)
-            return arr[0];
+            System.out.println("Find Max Sum Using DP .........:::===>" + arr[0]);
         if (n == 2)
-            return Math.max(arr[0], arr[1]);
+            System.out.println("Find Max Sum Using DP .........:::===>" + Math.max(arr[0], arr[1]));
 
         int[] DP = new int[n];
         DP[0] = arr[0];
@@ -36,32 +26,7 @@ public class MaximumSumSuchThatNo2ElementsAreAdjacent {
         for (int i = 2; i < arr.length; i++) {
             DP[i] = Math.max((arr[i] + DP[i - 2]), DP[i - 1]);
         }
-        return DP[n - 1];
-    }
-
-    public static void findMaxSumOptimized(int[] arr) {
-        int evenSum = 0, oddSum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (i % 2 == 0) {
-                evenSum += arr[i];
-            } else {
-                oddSum += arr[i];
-            }
-        }
-        System.out.println(evenSum > oddSum ? evenSum : oddSum);
-    }
-
-    public static void findMaximumSum(int[] arr) {
-        int sum = 0, maxSum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = 0;
-            for (int j = i; j < arr.length; j += 2) {
-                sum += arr[j];
-            }
-            if (maxSum < sum) {
-                maxSum = sum;
-            }
-        }
-        System.out.println(maxSum);
+        ArrayRotation.printArray(DP);
+        System.out.println("Find Max Sum Using DP .........:::===>" + DP[n - 1]);
     }
 }
