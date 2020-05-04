de5aeb0bb9485c20c4a3cd1ae2076fbe4a6f6403
Neeraj Jain <neeraj_jain@apple.com>
2020-05-04 23:02:21 +0530

    Added PartitionToKEqualSumSubset problem, Recursive, Memorization and Bottom Up solutions.

---
+++ src/com/leetcode/year_2020/DP/PartitionToKEqualSumSubset.java
@@ -0,0 +1,45 @@
+package com.leetcode.year_2020.DP;
+
+/**
+ * @author neeraj on 04/05/20
+ * Copyright (c) 2019, data-structures.
+ * All rights reserved.
+ */
+public class PartitionToKEqualSumSubset {
+
+    public static void main(String[] args) {
+        System.out.println(canPartitionKSubsets(new int[]{4, 3, 2, 3, 5, 2, 1}, 5));
+    }
+
+    public static boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if (k == 0 || sum % k != 0) return false;
+        return canPartitionKSubsets(nums, k, new boolean[nums.length], 0, 0, sum / k);
+    }
+
+    public static boolean canPartitionKSubsets(int[] nums, int bucket, boolean[] visited,
+                                               int startIndex, int currentSum, int targetSum) {
+        if (bucket == 1) { // If we have just 1 bucket so nums must be representing only those items.
+            // hence we can partition into 1 subset, which is whole nums.
+            return true;
+        }
+
+        if (currentSum == targetSum) { // This means we have completely filled the bucket.
+            return canPartitionKSubsets(nums, bucket - 1, visited, 0, 0, targetSum);
+        }
+
+        for (int i = startIndex; i < nums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                if (canPartitionKSubsets(nums, bucket, visited, i + 1, currentSum + nums[i], targetSum)) {
+                    return true;
+                }
+                visited[i] = false;
+            }
+        }
+        return false; // We did not found any such subset where target sum can be achieved.
+    }
+}
