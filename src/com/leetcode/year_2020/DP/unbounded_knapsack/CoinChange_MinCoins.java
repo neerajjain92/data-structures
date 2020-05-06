b74a768c85a0ef205011a278b4fbea5a00ec8fd5
Neeraj Jain <neeraj_jain@apple.com>
2020-05-06 23:42:29 +0530

    Added CoinChange_MinCoins and CoinChange_NumberOfWays problem solution

---
+++ src/com/leetcode/year_2020/DP/unbounded_knapsack/CoinChange_MinCoins.java
@@ -0,0 +1,54 @@
+package com.leetcode.year_2020.DP.unbounded_knapsack;
+
+import java.util.Arrays;
+
+/**
+ * @author neeraj on 06/05/20
+ * Copyright (c) 2019, data-structures.
+ * All rights reserved.
+ */
+public class CoinChange_MinCoins {
+
+    public static void main(String[] args) {
+        System.out.println(coinChange(new int[]{1, 2, 3}, 5));
+        System.out.println(coinChange(new int[]{1, 2, 5}, 11));
+        System.out.println(coinChange(new int[]{2}, 3));
+        System.out.println(coinChange(new int[]{470, 18, 66, 301, 403, 112, 360}, 8235));
+    }
+
+    static int t[][]; // Memorization Cache
+
+    public static int coinChange(int[] coins, int amount) {
+        t = new int[coins.length + 1][amount + 1];
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+        int minCoins = coinChangeMinimumNumbersOfCoinsRecursively(coins, coins.length - 1, amount);
+        return minCoins != Integer.MAX_VALUE - 1 ? minCoins : -1;
+    }
+
+    public static int coinChangeMinimumNumbersOfCoinsRecursively(int[] coins, int n, int amount) {
+        /**
+         * Here we want to get minimumNumber of coins, so our base condition will differ from
+         * {@link CoinChange_NumberOfWays} problem.
+         */
+        if (amount == 0) { // When we have to achieve amount = 0.
+            return 0;
+        }
+        if (n < 0) { // When we have no items then for sure we can't achieve any sum, so minimum coins tends to INFINITY.
+            return Integer.MAX_VALUE - 1;
+        }
+
+        if (t[n][amount] > -1) return t[n][amount];
+
+        if (coins[n] <= amount) { // We can include this coin to achieve the amount.
+            return t[n][amount] = Math.min(
+                    // We choose the coin, added it's occurrence as 1, and now let's reduce the amount but not the "n" (since we have unlimited supply)
+                    1 + coinChangeMinimumNumbersOfCoinsRecursively(coins, n, amount - coins[n]),
+                    coinChangeMinimumNumbersOfCoinsRecursively(coins, n - 1, amount)
+            );
+        } else {
+            return t[n][amount] = coinChangeMinimumNumbersOfCoinsRecursively(coins, n - 1, amount);
+        }
+    }
+}
---
+++ src/com/leetcode/year_2020/DP/unbounded_knapsack/CoinChange_NumberOfWays.java
@@ -0,0 +1,84 @@
+package com.leetcode.year_2020.DP.unbounded_knapsack;
+
+import com.leetcode.year_2020.DP.zero_one_knapsack.CountOfSubsetSumWithAGivenSum;
+
+import java.util.Arrays;
+
+/**
+ * @author neeraj on 06/05/20
+ * Copyright (c) 2019, data-structures.
+ * All rights reserved.
+ */
+@SuppressWarnings("DuplicatedCode")
+public class CoinChange_NumberOfWays {
+
+    public static void main(String[] args) {
+        System.out.println(change(5, new int[]{1, 2, 5}));
+        System.out.println(change(3, new int[]{2}));
+    }
+
+
+    /**
+     * This problem is similar to {@link CountOfSubsetSumWithAGivenSum}, the only difference is
+     * here we can take the coin multiple time(this is unbounded Knapsack) since we have limited supply.
+     */
+    static int t[][];
+
+    public static int change(int amount, int[] coins) {
+
+        t = new int[coins.length + 1][amount + 1]; // Memorization matrix.
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+        return findNoOfWaysWeCanMakeTheAmount
+                (coins, coins.length - 1, amount);
+        // return noOfWaysBottomUp(coins, amount);
+    }
+
+    public static int findNoOfWaysWeCanMakeTheAmount(int[] coins, int n, int amount) {
+        if (amount == 0) return 1; // If we have to make the amount of 0, we can make it without choosing any coin.
+        if (n < 0 || amount < 0)
+            return 0; // If we have no coins or we have to make negative amount that's never possible.
+
+        if (t[n][amount] > -1) return t[n][amount]; // Return from cache.
+
+        if (coins[n] <= amount) { // This coin can contribute
+            // to build up the total Amount.
+            return t[n][amount] = findNoOfWaysWeCanMakeTheAmount(coins, n, amount - coins[n]) // Choose the coin, but we can again choose it, hence not reducing n
+                    + findNoOfWaysWeCanMakeTheAmount(coins, n - 1, amount); // Not Choose the coin.
+
+        } else {
+            return t[n][amount] = findNoOfWaysWeCanMakeTheAmount(coins, n - 1, amount); // Not choosing the coin.
+        }
+    }
+
+    public static int noOfWaysBottomUp(int[] coins, int amount) {
+        int dp[][] = new int[coins.length + 1][amount + 1]; // Bottom up matrix to hold
+
+        // Initialize the matrix.
+        // i represents coin and j represents amount, in any cell.
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                if (i == 0) { // We don't have any coins.
+                    dp[i][j] = 0;
+                }
+                if (j == 0) { // We have to achieve sum of 0, which can be made by not choosing any coin.
+                    dp[i][j] = 1;
+                }
+            }
+        }
+
+        // Let's complete remaining matrix
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = dp[i][j - coins[i - 1]] + // Choosing the coin
+                            dp[i - 1][j]; // and not choosing the coin.
+                } else {
+                    dp[i][j] = dp[i - 1][j]; // not choosing the coin.
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
