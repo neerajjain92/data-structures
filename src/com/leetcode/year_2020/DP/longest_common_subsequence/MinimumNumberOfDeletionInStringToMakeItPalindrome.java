66b68f30b5b1f4b8b6115028d292a0b14323b473
Neeraj Jain <neeraj_jain@apple.com>
2020-05-08 01:53:16 +0530

    Solved Minimum Number of Deletion in a String to Make It Palindrome

---
+++ src/com/leetcode/year_2020/DP/longest_common_subsequence/MinimumNumberOfDeletionInStringToMakeItPalindrome.java
@@ -0,0 +1,46 @@
+package com.leetcode.year_2020.DP.longest_common_subsequence;
+
+import static com.leetcode.year_2020.DP.longest_common_subsequence.LongestPalindromicSubsequence.longestPalindromeSubseq;
+
+/**
+ * @author neeraj on 08/05/20
+ * Copyright (c) 2019, data-structures.
+ * All rights reserved.
+ */
+public class MinimumNumberOfDeletionInStringToMakeItPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(findMinDeletionsToMakePalindrome("aebcbda"));
+        System.out.println(findMinDeletionsToMakePalindrome("geeksforgeeks"));
+    }
+
+    /**
+     * Input : aebcbda
+     * Output : 2
+     * Remove characters 'e' and 'd'
+     * Resultant string will be 'abcba'
+     * which is a palindromic string
+     * <p>
+     * Input : geeksforgeeks
+     * Output : 8
+     */
+    public static int findMinDeletionsToMakePalindrome(String str) {
+        /**
+         * Okay so this problem is kindaa need some help from
+         * Longest Palindromic Subsequence, why ?
+         *
+         * I/p : a e b c b d a    ===> What is the LPS here : a b c b a
+         * , what's left is just junk for us and can be deleted, to make the complete string
+         * a Palindrome.
+         *
+         * Now based on {@link LongestPalindromicSubsequence} we know it can be solved using
+         * LCS(Longest Common Sequence) approach. By taking reverseOf(Input1) as input2.
+         *
+         * So our solution will be
+         * MIN_DELETIONS = str.length - LCS(str1, reverse(str2));
+         */
+        int MIN_DELETIONS = str.length() - longestPalindromeSubseq(str);
+        return MIN_DELETIONS;
+    }
+
+}
