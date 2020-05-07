51877b62f0cfa6e41a383bdb4246a3ca5448a362
Neeraj Jain <neeraj_jain@apple.com>
2020-05-07 20:37:00 +0530

    Added ShortestCommonSuperSequence code.

---
+++ src/com/leetcode/year_2020/DP/ShortestCommonSuperSequence.java
@@ -0,0 +1,39 @@
+package com.leetcode.year_2020.DP;
+
+import com.leetcode.year_2020.DP.longest_common_subsequence.LengthOfLongestCommonSubsequence;
+
+/**
+ * @author neeraj on 07/05/20
+ * Copyright (c) 2019, data-structures.
+ * All rights reserved.
+ */
+public class ShortestCommonSuperSequence {
+
+    public static void main(String[] args) {
+        System.out.println("ShortestCommonSuperSequence Length is " + findLengthOfShortestCommonSuperSequence("GEEKE", "EKE"));
+        System.out.println("ShortestCommonSuperSequence Length is " + findLengthOfShortestCommonSuperSequence("AGGTAB", "GXTXAYB"));
+    }
+
+    public static int findLengthOfShortestCommonSuperSequence(String str1, String str2) {
+        /**
+         * s1 ===> A G G T A B
+         * s2 ==>  G X T X A Y B
+         *
+         * what will be the super sequence of s1 and s2 ==> s1 + s2
+         * A G G T A B G X T X A Y B
+         *
+         * but question is asking for Shortest Common SuperSequence.
+         * So let's include common items just once from both s1 and s2;
+         *
+         * A G G X T X A Y B ==> Shortest Common SuperSequence.
+         *
+         * Now what were those common items.==> G T A B (Now if we notice carefully what is this it's just the LCS).
+         *
+         * So what can we do to find the length of Shortest Common SuperSequence
+         *
+         *  length of ShortestCommonSuperSequence = Length(S1) + Length(S2) - LengthOfLCS
+         *
+         */
+        return str1.length() + str2.length() - LengthOfLongestCommonSubsequence.findLengthOfLCS(str1, str2);
+    }
+}
