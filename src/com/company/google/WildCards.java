a7f0f98e8913b42a9b42f31d4bd1d1a47766e275
Neeraj Jain <neeraj_jain@apple.com>
2020-05-13 01:02:16 +0530

    Updated Google's WildCard problem description

--- src/com/company/google/WildCards.java
+++ src/com/company/google/WildCards.java
@@ -2,6 +2,25 @@ package com.company.google;
 
 /**
  * Google Interview Problem.
+ * <p>
+ * Wildcards
+ * Have the function wildcards(str) read str which will contain two strings separated by a space
+ * The first string will consist of the following sets of characters: +, $, and {N} which is optional
+ * '+' character represents a single alphabetic character,
+ * '$' character represents a number between 1-9,
+ * asterisk '*' represents a sequence of the same character of length 3 unless it is followed by
+ * ({N} which represents how many characters should appear in the sequence where N will be at least 1.
+ * <p>
+ * Your goal is to determine if the second string exactly matches the pattern of the first string in the input
+ * <p>
+ * For example if str is '++*{5} jtggggg" then the second string in this case does match the pattern,
+ * so your program should return the string true If the second string does not match the pattern your program should
+ * return the string false.
+ * Examples
+ * <p>
+ * input: "+++++* abcdehhhhh" Output: false
+ * Input: "$**+*{2} 9mmmrrrkbb" Output: true
+ * Browse Resources powered by Google
  *
  * @author neeraj on 12/05/20
  * Copyright (c) 2019, data-structures.
