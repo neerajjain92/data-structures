030b31c67083aabe2254750edced5fd89301d880
Neeraj Jain <neeraj_jain@apple.com>
2020-05-16 11:54:00 +0530

    Solved Maximum Sum Circular SubArray

--- src/com/leetcode/year_2020/DP/unbounded_knapsack/CoinChange_MinCoins.java
+++ src/com/leetcode/year_2020/DP/unbounded_knapsack/CoinChange_MinCoins.java
@@ -12,6 +12,7 @@ public class CoinChange_MinCoins {
     public static void main(String[] args) {
         System.out.println(coinChange(new int[]{1, 2, 3}, 5));
         System.out.println(coinChange(new int[]{1, 2, 5}, 11));
+        System.out.println(coinChange(new int[]{1,5,6,9}, 11));
         System.out.println(coinChange(new int[]{2}, 3));
         System.out.println(coinChange(new int[]{470, 18, 66, 301, 403, 112, 360}, 8235));
     }
