0defb0756f4508916322b1eaabd80e651ea8dcd4
Neeraj Jain <neeraj_jain@apple.com>
2020-05-23 23:59:16 +0530

    Solved Task Scheduler problem

--- src/com/leetcode/year_2020/Greedy/TaskScheduler.java
+++ src/com/leetcode/year_2020/Greedy/TaskScheduler.java
@@ -1,11 +1,9 @@
 package com.leetcode.year_2020.Greedy;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 /**
+ * https://leetcode.com/problems/task-scheduler/
  * @author neeraj on 23/05/20
  * Copyright (c) 2019, data-structures.
  * All rights reserved.
@@ -14,52 +12,85 @@ public class TaskScheduler {
 
     public static void main(String[] args) {
         System.out.println(leastInterval(new char[]{
-                'A','A','A','B','B','B'
+                'A', 'A', 'A', 'B', 'B', 'B'
         }, 0));
+
+        System.out.println(leastInterval(new char[]{
+                'A', 'A', 'A', 'B', 'B', 'B'
+        }, 2));
+        System.out.println(leastInterval(new char[]{
+                'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'
+        }, 2));
+        System.out.println(leastInterval(new char[]{
+                'A','A','A','A','A','A','B','C','D','E','F','G'
+        }, 1));
     }
 
     public static int leastInterval(char[] tasks, int n) {
         Map<Character, Integer> taskFreq = new HashMap<>();
-        for(char c: tasks) {
-            taskFreq.put(c, taskFreq.getOrDefault(c, 0)+1);
+        for (char c : tasks) {
+            taskFreq.put(c, taskFreq.getOrDefault(c, 0) + 1);
         }
         Map<Character, Integer> taskLastProcessedIndex = new HashMap<>();
-        Queue<Character> queue = new LinkedList<>();
-        queue.addAll(taskFreq.keySet());
 
-        int currentTaskInProcessCounter = 0;
-        Character polledTask = null;
-        while(!queue.isEmpty()) {
-            polledTask = null;
+        // We need a priority queue
+        // we want high frequency item on top.
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>(Comparator.comparingInt(A -> -taskFreq.get(A)));
+        maxHeap.addAll(taskFreq.keySet());
 
-            // Task Already exist
-            // Let's compare when it last executed.
-            if(taskLastProcessedIndex.containsKey(queue.peek())) {
-                if(currentTaskInProcessCounter - taskLastProcessedIndex
-                        .get(queue.peek()) > n) {
-                    polledTask = queue.poll();
+        Stack<Character> stack = new Stack<>();
+
+        int timer = 1;
+        while (!maxHeap.isEmpty()) {
+            char item = maxHeap.poll();
+
+            // Item was already processed.
+            if (taskLastProcessedIndex.containsKey(item)) {
+                int diff = timer - taskLastProcessedIndex.get(item);
+                if (diff > n) { // diff > allowed difference.
+                    addItemToHeapIfRequired(taskFreq, item, maxHeap);
+                    taskLastProcessedIndex.put(item, timer);
+                } else {
+                    while (!maxHeap.isEmpty()) {
+                        char itemOnTop = maxHeap.peek();
+                        if (taskLastProcessedIndex.containsKey(itemOnTop)) {
+                            diff = timer - taskLastProcessedIndex.get(itemOnTop);
+                            if (diff > n) {
+                                itemOnTop = maxHeap.poll();
+                                addItemToHeapIfRequired(taskFreq, itemOnTop, maxHeap);
+                                break;
+                            } else {
+                                stack.push(maxHeap.poll());
+                            }
+                        } else {
+                            itemOnTop = maxHeap.poll();
+                            taskLastProcessedIndex.put(itemOnTop, timer);
+                            addItemToHeapIfRequired(taskFreq, itemOnTop, maxHeap);
+                            break;
+                        }
+                    }
+                    while (!stack.isEmpty()) {
+                        maxHeap.add(stack.pop());
+                    }
+                    maxHeap.add(item);
                 }
-            } else {
-                // First Time Task Encountered.
-                polledTask = queue.poll();
-                taskLastProcessedIndex.put(polledTask, currentTaskInProcessCounter);
+            } else { // encountering the item for the first time
+                taskLastProcessedIndex.put(item, timer);
+                addItemToHeapIfRequired(taskFreq, item, maxHeap);
             }
-
-            // Decrement the freq.
-            if(polledTask!= null) {
-                taskFreq.put(polledTask, taskFreq.get(polledTask) - 1);
-                if(taskFreq.get(polledTask) > 0) {
-                    queue.add(polledTask);
-                }
+            if (!maxHeap.isEmpty()) {
+                timer++;
             }
-
-            if(!queue.isEmpty()) {
-                taskLastProcessedIndex.put(polledTask, currentTaskInProcessCounter);
-            }
-            // Increment the Task Counter regardless of whether task got polled off.
-            currentTaskInProcessCounter++;
-            System.out.println("currentTaskInProcessCounter value is "+ currentTaskInProcessCounter);
         }
-        return currentTaskInProcessCounter;
+        return timer;
+    }
+
+    private static void addItemToHeapIfRequired(Map<Character, Integer> taskFreq,
+                                                char item, PriorityQueue<Character> maxHeap) {
+        int itemsFreqLeftToBeProcessed = taskFreq.get(item) - 1;
+        if (itemsFreqLeftToBeProcessed > 0) {
+            taskFreq.put(item, itemsFreqLeftToBeProcessed);
+            maxHeap.add(item);
+        }
     }
 }
