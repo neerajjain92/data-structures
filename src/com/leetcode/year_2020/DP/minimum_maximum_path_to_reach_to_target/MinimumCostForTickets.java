d97f5350ad5bb6a5b44c940c192d2baa322c9b58
Neeraj Jain <neeraj_jain@apple.com>
2020-06-06 01:41:07 +0530

    Solved more minimum_maximum_reach_target_kind_problems

---
+++ src/com/leetcode/year_2020/DP/minimum_maximum_path_to_reach_to_target/MinimumCostForTickets.java
@@ -0,0 +1,62 @@
+package com.leetcode.year_2020.DP.minimum_maximum_path_to_reach_to_target;
+
+import java.util.Arrays;
+
+/**
+ * @author neeraj on 05/06/20
+ * Copyright (c) 2019, data-structures.
+ * All rights reserved.
+ */
+public class MinimumCostForTickets {
+
+    public static void main(String[] args) {
+        System.out.println(mincostTickets(
+                new int[]{1, 4, 6, 7, 8, 20},
+                new int[]{2, 7, 15}));
+
+    }
+
+    static int[] T;
+
+    public static int mincostTickets(int[] days, int[] costs) {
+        /**
+         * So we can first do a DFS and try purchasing exhaustive combination
+         * of passes
+         */
+        // Initially since i have not purchased any ticket hence there is no valid day in which i can travel without pass.
+        T = new int[days[days.length - 1]];
+        Arrays.fill(T, -1);
+        return doDFS(days, costs, 0, 0);
+    }
+
+    private static int doDFS(int[] days, int[] costs, int ithDay, int travelCoveredUntil) {
+        /**
+         * Ok, at the days[i], how many possibilities that we can do?
+         *
+         * pay nothing. We have bought a week-pass, or a month-pass in the past, and up-to-now, the pass is still valid.
+         * buy a day-pass to cover only this day
+         * buy a week-pass to cover next 6 consecutive days.
+         * buy a month-pass to cover next 29 consecutive days.
+         */
+
+
+        if (ithDay == days.length) return 0; // We don't have any days to travel.
+
+        // Here pass is still valid.
+        if (days[ithDay] <= travelCoveredUntil) {
+            return doDFS(days, costs, ithDay + 1, travelCoveredUntil);
+        }
+
+        // If we have the data in cache.
+        if (T[ithDay] != -1 && days[ithDay] > travelCoveredUntil) return T[ithDay];
+
+
+        // Since we are not covered with any previous day.
+        // so we can take any of these 3 passes.
+        int costOf1DayPass = costs[0] + doDFS(days, costs, ithDay + 1, days[ithDay]);
+        int costOf7DayPass = costs[1] + doDFS(days, costs, ithDay + 1, days[ithDay] + 6);
+        int costOfMonthPass = costs[2] + doDFS(days, costs, ithDay + 1, days[ithDay] + 29);
+
+        return T[ithDay] = Math.min(costOf1DayPass, Math.min(costOf7DayPass, costOfMonthPass));
+    }
+}
---
+++ src/com/leetcode/year_2020/june_challenge/week1/RandomPickwithWeight.java
@@ -0,0 +1,144 @@
+package com.leetcode.year_2020.june_challenge.week1;
+
+import com.company.google.RandomStateNameGeneratorBasedOnProportionateToProbabilityOfTotalPopulation;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @author neeraj on 05/06/20
+ * Copyright (c) 2019, data-structures.
+ * All rights reserved.
+ */
+public class RandomPickwithWeight {
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
+    public static void main(String[] args) {
+        /**
+         * This problem is similar to {@link RandomStateNameGeneratorBasedOnProportionateToProbabilityOfTotalPopulation}
+         * problem, where we will calculate the probability based on state population, here we can calculate them based
+         * on weight of the input. And then divide the slot
+         */
+
+        int[] w = new int[]{2, 5, 3, 4};
+//        Solution solution = new Solution(w);
+        SolutionUsingBinarySearchAndPrefixSum solution = new SolutionUsingBinarySearchAndPrefixSum(w);
+        Map<Integer, Integer> weightAndItsFrequency = new HashMap<>();
+        for (int i = 0; i < 10000; i++) {
+            int index = solution.pickIndex();
+            int actualWeight = w[index];
+            weightAndItsFrequency.put(actualWeight, weightAndItsFrequency.getOrDefault(actualWeight, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : weightAndItsFrequency.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + (Double.parseDouble(df.format(entry.getValue() / 10000d)) * 100) + " %");
+        }
+
+
+        /**
+         * Okay so there is  another approach as well using which we can solve this problem
+         * via prefix sum and binary search
+         */
+    }
+
+
+}
+
+class SolutionUsingBinarySearchAndPrefixSum {
+
+    int[] prefixSum;
+    Random random;
+
+    public SolutionUsingBinarySearchAndPrefixSum(int[] w) {
+        /**
+         * w[] = {2,5,3,4}
+         * prefixSum[] = {2,7,10,14}
+         */
+        this.random = new Random();
+        prefixSum = new int[w.length];
+        prefixSum[0] = w[0];
+        for (int i = 1; i < w.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + w[i];
+        }
+    }
+
+    public int pickIndex() {
+        /**
+         * Assume we want to get the
+         * w[] = {2,5,3,4}
+         * prefixSum[] = {2,7,10,14}
+         *
+         * We want random value from 1 to 14...
+         * so that we can divide values from
+         * 0 to 2 -----to --> index 0
+         * 3 to 7 ----- to ----> index 1
+         * 8 to 10 ----- to-----> index 2
+         * 11 to 14 ----- to ------> index 3
+         */
+        int len = prefixSum.length;
+        int index = random.nextInt(prefixSum[len - 1]) + 1;
+        int left = 0;
+        int right = len - 1;
+        ;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (prefixSum[mid] == index) {
+                return mid;
+            }
+
+            if (prefixSum[mid] < index) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
+
+class Solution {
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
+    double[] probabilityProportion;
+    Map<Double, Integer> probabilityAndItsIndex = new HashMap<>();
+    int[] w;
+
+    public Solution(int[] w) {
+        Arrays.sort(w); // Sorting weights in ascending order and we will use then in reverse order.
+        this.w = w;
+        int totalWeight = 0;
+        for (int i : w) {
+            totalWeight += i;
+        }
+
+        probabilityProportion = new double[w.length];
+        double previousWeightage = 0.0d;
+        for (int i = w.length - 1; i >= 0; i--) {
+            probabilityProportion[w.length - 1 - i] = previousWeightage + (w[i]) / (double) totalWeight;
+            probabilityAndItsIndex.putIfAbsent(probabilityProportion[w.length - 1 - i], i);
+            previousWeightage = probabilityProportion[w.length - 1 - i];
+        }
+
+        for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i] + " -> " +
+                    (Double.parseDouble(df.format(w[i] / Double.valueOf(totalWeight))) * 100) + " %");
+        }
+        System.out.println("==========================OUTPUT with its percentage=========================");
+    }
+
+    public int pickIndex() {
+        double random = Math.random(); // equally distributed random value.
+        double previousWeightage = 0.0d;
+        for (int i = 0; i < probabilityProportion.length; i++) {
+            if (random >= previousWeightage && random <= probabilityProportion[i]) {
+                return probabilityAndItsIndex.get(probabilityProportion[i]);
+            }
+            previousWeightage = probabilityProportion[i];
+        }
+        return -1;
+    }
+}
